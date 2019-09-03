086060165bccd6acab8f480c2f6b8423ffb57251
Allan96 <allan.souza.silva@usp.br>
2019-09-03 04:01:28 -0300

    API pronta

--- API/index.js
+++ API/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -13,11 +9,53 @@ app.use(bodyParser.json());
 
 const router = express.Router();
 router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
-app.use('/', router)
+app.use('/', router);
 
 
-//inicia o servidor
+//INICIA O SERVIDOR
 app.listen(port);
 console.log('API funcionando!');
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+//FAZ A CONEXÃO PARA SE FAZER AS CONSULTAS NO BANCO DE DADOS
+function execSQLQuery(sqlQry, res) {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '',
+        database: 'emailapi'
+    });
+
+    connection.query(sqlQry, function(error, results, fields) {
+        if (error)
+            res.json(error);
+        else
+            res.json(results);
+        connection.end();
+        console.log('executou!');
+    });
+}
+
+//MOSTRA TODOS OS CLIENTES
+router.get('/clientes', (req, res) => {
+    execSQLQuery('SELECT * FROM testenode', res);
+});
+
+//MOSTRA CLIENTES ESPECIFICOS
+router.get('/clientes/:id?', (req, res) => {
+    let filter = '';
+    if (req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM testenode' + filter, res);
+});
+
+//DELETA UM USER
+router.delete('/clienteD/:id', (req, res) => {
+    execSQLQuery('DELETE FROM testenode WHERE ID=' + parseInt(req.params.id), res);
+});
+
+//INSERE UM USER
+router.post('/clientes', (req, res) => {
+    const nome = req.body.nome.substring(0, 150);
+    const cpf = req.body.cpf.substring(0, 11);
+    execSQLQuery(`INSERT INTO testenode(Nome, CPF) VALUES('${nome}','${cpf}')`, res);
+});
