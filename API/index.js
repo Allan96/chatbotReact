1cf027bac0fc51eeeeb4a4be5000a84710788953
Allan96 <allan.souza.silva@usp.br>
2019-09-03 04:19:01 -0300

    Autenticação JWT

--- API/index.js
+++ API/index.js
@@ -3,6 +3,37 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3000; //porta padrão
 const mysql = require('mysql');
+var jwt = require('jsonwebtoken');
+
+app.post('/login', (req, res, next) => {
+    if (req.body.user === 'luiz' && req.body.pwd === '123') {
+        //auth ok
+        const id = 1; //esse id viria do banco de dados
+        var token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: 300 // expires in 5min
+        });
+        res.status(200).send({ auth: true, token: token });
+    }
+
+    res.status(500).send('Login inválido!');
+});
+
+app.get('/logout', function(req, res) {
+    res.status(200).send({ auth: false, token: null });
+});
+
+function verifyJWT(req, res, next) {
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+    jwt.verify(token, process.env.SECRET, function(err, decoded) {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+        // se tudo estiver ok, salva no request para uso posterior
+        req.userId = decoded.id;
+        next();
+    });
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -37,12 +68,12 @@ function execSQLQuery(sqlQry, res) {
 }
 
 //MOSTRA TODOS OS CLIENTES
-router.get('/clientes', (req, res) => {
+router.get('/clientes', verifyJWT, (req, res) => {
     execSQLQuery('SELECT * FROM testenode', res);
 });
 
 //MOSTRA CLIENTES ESPECIFICOS
-router.get('/clientes/:id?', (req, res) => {
+router.get('/clientes/:id?', verifyJWT, (req, res) => {
     let filter = '';
     if (req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
     execSQLQuery('SELECT * FROM testenode' + filter, res);
