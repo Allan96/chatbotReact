b86231c96b176b1fd9b8bd106be85af802b42cc5
Allan96 <allan.souza.silva@usp.br>
2019-09-04 23:56:05 -0300

    'bootstrap'

--- interface/src/components/App.js
+++ interface/src/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Badge } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-   aaa
-    </div>
+    <div>
+    <Badge color="primary">Primary</Badge>
+    <Badge color="secondary">Secondary</Badge>
+    <Badge color="success">Success</Badge>
+    <Badge color="danger">Danger</Badge>
+    <Badge color="warning">Warning</Badge>
+    <Badge color="info">Info</Badge>
+    <Badge color="light">Light</Badge>
+    <Badge color="dark">Dark</Badge>
+  </div>
   );
 }
 
